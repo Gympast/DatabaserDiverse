9dcfcf47c8a6c15aae96e2dde26988946604d7b7
Gympast <112376007+Gympast@users.noreply.github.com>
2022-11-15 20:23:38 +0100

    Update JohannesIntro.sql

---
+++ SQLTemp/JohannesIntro.sql
@@ -0,0 +1,28 @@
+--CREATE DATABASE KlassenDb;
+
+USE KlassenDb;
+
+--CREATE TABLE KlassenTBL(
+--[Id] int,
+--[Förnamn] nvarchar(max) NOT NULL,
+--[Efternamn] nvarchar(max) NOT NULL,
+--[Ålder] int,
+--[Hemort] nvarchar(max) NOT NULL
+--Primary Key (Id)
+--);
+
+--INSERT INTO KlassenTBL
+--VALUES	(1, 'Johannes', 'Fredriksson', 37, 'Frölunda'),
+--		(2, 'Jing', 'Leng', 5, 'Hisingen'),
+--		(3, 'Andreas', 'Nordfeldt', null, 'Hisingen'),
+--		(4, 'Linus', 'Fjellström', null, 'Mölndal');
+
+UPDATE KlassenTBL
+SET [Ålder] = 31,
+	[Hemort] = 'Göteborg'
+WHERE [Id] = 3;
+
+SELECT [Förnamn] FROM KlassenTBL
+WHERE [Förnamn] LIKE '%n%';
+
+SELECT * FROM KlassenTBL;
